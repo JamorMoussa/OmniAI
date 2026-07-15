4be7c3fb98edaff7b92bec5968fe60b2b4f07fd5
JamorMoussa <moussajamorsup@gmail.com>
2026-07-16 00:26:35 +0100

    add /speech and /voices endpoints for kokoro model (#1)

--- omniai/services/omnitts/api.py
+++ omniai/services/omnitts/api.py
@@ -1,24 +1,41 @@
 from fastapi import FastAPI, APIRouter, Request
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
+from omnitts import KokoroTTS, Manifest
+    
+
+class SpeechRequest(BaseModel):
+    model: str 
+    input: str 
+    voice: str
+    # speed: int
 
 tts = FastAPI()
 
 router = APIRouter(prefix="/v1/audio")
 tts.include_router(router=router)
 
+speech_model = KokoroTTS.load(manifest=Manifest.load("/models/kokoro"))
 
-@router.get("/speech")
-def speech(
-    request: Request
-):
-    return {
-        "message": "hello from omnitts service"
-    }
 
+@router.post("/speech")
+async def speech(
+    request: SpeechRequest
+):  
+    audio_streams = speech_model.create_stream(
+        text= request.input, voice=request.voice
+    )
+
+    return StreamingResponse(
+        content= map(lambda stream: stream.as_bytes(), audio_streams),
+        media_type="audio/wav",
+    )
 
 @router.get("/voices")
 def voices(
     request: Request
 ):
     return {
-        "message": "voices api"
+        "voices": speech_model.voices()
     }
